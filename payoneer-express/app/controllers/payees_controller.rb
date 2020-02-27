608be1fe0435776d1bba6deb7d497eedfa530298
tomoyaueno29 <freemarket.sample.63a@gmail.com>
2020-02-27 14:25:00 +0900

    commit

--- payoneer-express/app/controllers/payees_controller.rb
+++ payoneer-express/app/controllers/payees_controller.rb
@@ -1,4 +1,7 @@
 class PayeesController < ApplicationController
+  before_action :set_payee, only: [:show, :edit, :update, :destroy, :pay,
+    :confirm]
+
   def index
     @payees = Payee.all
   end
@@ -7,6 +10,47 @@ class PayeesController < ApplicationController
   end
 
   def new
-    
+    @payee = Payee.new
+    @payee.balance = 20
+  end
+
+  def edit
+  end
+
+  def create
+    @payee = Payee.new(payee_params)
+
+    if @payee.save
+      redirect_to @payee, notice: 'Payee was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @payee.update(payee_params)
+      resirect_to @payee, notice: 'Payee was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @payee.destroy
+    redirect_to payees_url, notice: 'Payee was successfully destroyed.'
+  end
+
+  def pay
+
+  end
+
+  private
+
+  def set_payee
+    @payee = Payee.find(params[:id])
+  end
+
+  def payee_params
+    params.require(:payee).permit(:email, :balance, :currency_id)
   end
 end
